e47d90b54e5d0506659a387c2ebd137213e086dd
amirdev1883 <amirdev1883@gmail.com>
2024-11-15 18:20:46 +0330

    add urls for jwt

--- djangosocialmedia/api/urls.py
+++ djangosocialmedia/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('blog/', include(('djangosocialmedia.blog.urls', 'blog'))),
     path('users/', include(('djangosocialmedia.users.urls', 'users'))),
+    path('auth/', include(('djangosocialmedia.authentication.urls', 'auth'))),
 ]
