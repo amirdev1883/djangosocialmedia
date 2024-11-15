6bffcfca51a0ded678f357fbff7fb1d807e2c4bd
amirdev1883 <amirdev1883@gmail.com>
2024-11-15 21:23:52 +0330

    crate model Post and Subscription

--- djangosocialmedia/blog/models.py
+++ djangosocialmedia/blog/models.py
@@ -1,7 +1,43 @@
 from django.db import models
 from djangosocialmedia.common.models import BaseModel
+from djangosocialmedia.users.models import BaseUser
+from django.core.exceptions import ValidationError
 
 
-class Product(BaseModel):
-    name = models.TextField(max_length=255)
+
+class Post(models.Model):
+    slug = models.SlugField(
+        primary_key=True,
+        max_length=100
+    ) 
+    title = models.CharField(
+        max_length=100 ,
+        unique=True
+    ) 
+    content = models.CharField(
+        max_length=1000
+    ) 
+    author = models.ForeignKey(
+        BaseUser,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    def __str__(self):
+        return self.slug
+    
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(BaseUser, models.CASCADE, related_name='subs') 
+    target = models.ForeignKey(BaseUser, models.CASCADE, related_name='targets')
+
+    class Meta:
+        unique_together = ('subscriber', 'target')
+
+    def clean(self):
+        if self.subscriber == self.target:
+            raise ValidationError({"subscriber":("subscriber cannot be equal to target")})
+        
+    def __str__(self):
+        return f'{self.subscriber.email} sub {self.target.email}'
+
 
