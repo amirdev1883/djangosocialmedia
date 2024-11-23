1412fe39444ebc42a2686fea6baae20a6619f917
amirdev1883 <amirdev1883@gmail.com>
2024-11-23 20:00:10 +0330

    modified models in blog app djangosocialmedia/blog/models.py

--- djangosocialmedia/blog/models.py
+++ djangosocialmedia/blog/models.py
@@ -1,43 +1,42 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from djangosocialmedia.common.models import BaseModel
 from djangosocialmedia.users.models import BaseUser
-from django.core.exceptions import ValidationError
 
 
+class Post(BaseModel):
 
-class Post(models.Model):
     slug = models.SlugField(
-        primary_key=True,
-        max_length=100
-    ) 
-    title = models.CharField(
-        max_length=100 ,
-        unique=True
-    ) 
-    content = models.CharField(
-        max_length=1000
-    ) 
-    author = models.ForeignKey(
-        BaseUser,
-        on_delete=models.SET_NULL,
-        null=True
+            primary_key=True,
+            max_length=100,
     )
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    content = models.CharField(
+        max_length=1000,
+    )
+    author = models.ForeignKey(
+            BaseUser, 
+            on_delete=models.SET_NULL,
+            null=True,
+    )
+
     def __str__(self):
         return self.slug
-    
+
 
 class Subscription(models.Model):
-    subscriber = models.ForeignKey(BaseUser, models.CASCADE, related_name='subs') 
-    target = models.ForeignKey(BaseUser, models.CASCADE, related_name='targets')
+    subscriber = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name="subs")
+    target     = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name="targets")
 
     class Meta:
         unique_together = ('subscriber', 'target')
 
     def clean(self):
         if self.subscriber == self.target:
-            raise ValidationError({"subscriber":("subscriber cannot be equal to target")})
-        
+            raise ValidationError({"subscriber": ("subscriber cannot be equal to target")})
+
     def __str__(self):
-        return f'{self.subscriber.email} sub {self.target.email}'
-
-
+        return f"{self.subscriber.email}- {self.target.email}"
